ba573d34801618a4c4c958a92e68c2f7fa52a839
Nick H <nick.humrich@canopytax.com>
2015-09-23 15:37:31 -0600

    fix issues

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ ENDPOINT="https://app.wercker.com/api/v3/applications/$WERCKER_WAIT_FOR_BUILD_OW
 function get_status { curl -s -H "Content-type: application/json" -H "Authorization: Bearer $WERCKER_WAIT_FOR_BUILD_TOKEN" $ENDPOINT | ./jq '.[0] | .status'; }
 
 STATUS=$(get_status)
-while [ $STATUS != "\"finished\"" ]; do echo $STATUS; sleep 10; STATUS=$(get_status); done;
+while [ "$STATUS" != "\"finished\"" ]; do echo "$STATUS"; sleep 10; STATUS=$(get_status); done;
 echo "Dependent Build complete";
